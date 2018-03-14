bce2b84fe383a558c55f599f2649774c03ed6337
FelixTG <33939889+FelixTG@users.noreply.github.com>
2018-03-14 13:24:35 -0400

    Adding custom player

---
+++ Assets/Source_Code/CameraMouvement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouvement : MonoBehaviour {
+    public float verticalSpeed;
+    private float iniCamY;
+    public Vector3 positionCam;
+    // Use this for initialization
+    void Start () {
+        iniCamY = Input.mousePosition.y;
+        positionCam = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(-v, 0, 0);
+        positionCam = transform.position;
+    }
+}
---
+++ Assets/Source_Code/StunBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunBall : MonoBehaviour {
+    public float Speed;
+    // Use this for initialization
+    void Start()
+    {
+        transform.Translate(Speed, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Speed, 0, 0);
+    }
+}
