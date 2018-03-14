74d40219c18e10f5e6a13eba89b97bed82c55160
G-Lauz <gabriellauzier77@gmail.com>
2018-03-14 13:14:50 -0400

    Add scripts use by the monsters

---
+++ Assets/Source_Code/Mob.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Mob : Enemy
+{
+
+    private void Start()
+    {
+        
+    }
+
+    /*
+    void ChasePlayer(Player player)
+    {
+        this.player = player;
+        navAgent.SetDestination(player.transform.position);
+    }*/
+
+    // Update is called once per frame
+    /*void Update () {
+		
+	}*/
+}
---
+++ Assets/Source_Code/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int life { get; set; }
+    public int maxLife { get; set; }
+
+    // Use this for initialization
+    void Start () {
+         
+	}   
+
+    public void lostLife()
+    {
+        //TODO
+        if (life <= 0)
+            wakeUp();
+    }
+
+    public void wakeUp()
+    {
+        //TODO
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
