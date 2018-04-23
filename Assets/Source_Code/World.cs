926afb781a6ff13b1fd97cbe38894aabaec338b3
FelixTG <33939889+FelixTG@users.noreply.github.com>
2018-04-23 09:19:53 -0400

    fixing maxence

--- Assets/Source_Code/World.cs
+++ Assets/Source_Code/World.cs
@@ -206,7 +206,7 @@ public class World : MonoBehaviour
         // CHUNKS
         this.chunks = new Chunk[mapSize / chunkSize, mapSize / chunkSize];
 
-        this.GenerateWorldValues(3);
+        this.GenerateWorldValues(Random.Range(0,10));
         this.GenerateTerrainValues(Random.Range(0, 3));
         this.GenerateSurfaceHeights();
         this.GenerateTerrain();
@@ -222,7 +222,7 @@ public class World : MonoBehaviour
         this.GenerateLife();
 
         // MONSTERS
-        this.monsters = new GameObject[1];
+        this.monsters = new GameObject[this.numberOfMonsters];
         this.GenerateMonsters();
 
         // TREES
