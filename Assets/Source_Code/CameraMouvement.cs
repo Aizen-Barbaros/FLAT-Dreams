8fbfeafbce67703b5794ee737a15d49ff4c7b444
asarry <arthur.sarry@gmail.com>
2018-04-24 17:36:10 -0500

    Restricting the camera rotation

--- Assets/Source_Code/CameraMouvement.cs
+++ Assets/Source_Code/CameraMouvement.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class CameraMouvement : MonoBehaviour
 {
+    private float rotationY;
     private float verticalSpeed;
-    private Vector3 positionCam;
 
     void Start ()
     {
+        this.rotationY = 0.0f;
         this.verticalSpeed = 7.5f;
-        this.positionCam = transform.position;
     }
 
     void Update ()
     {
-        transform.Rotate(-this.verticalSpeed * Input.GetAxis("Mouse Y"), 0, 0);
-        positionCam = transform.position;
+        this.rotationY += Input.GetAxis("Mouse Y") * this.verticalSpeed;
+        transform.localEulerAngles = new Vector3(-Mathf.Clamp(this.rotationY, -90, 90), transform.localEulerAngles.y, transform.localEulerAngles.z);
     }
 
 
     public void SetVerticalSpeed(float verticalSpeed)
     {
         this.verticalSpeed = verticalSpeed;
+    }
+
+
+    public float ClampAngle(float angle, float min, float max)
+    {
+        if (min < 0)
+            min += 360;
+        if (angle > 180)
+            return Mathf.Clamp(angle, min, 360);
+        else
+            return Mathf.Clamp(angle, 0, max);
     }
 }
--- Assets/Source_Code/Player.cs
+++ Assets/Source_Code/Player.cs
@@ -127,10 +127,7 @@ public class Player : Character
         }
 
         if (collision.gameObject.tag == "Ennemy")
-        {
-            //this.currentLives--;
             this.isCaught = true;
-        }
     }
 
 
