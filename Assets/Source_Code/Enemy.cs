ecd013070dc0800c173dca0c38c07b16d9602d8d
G-Lauz <gabriellauzier77@gmail.com>
2018-04-29 12:29:37 -0400

    mini comment 2

--- Assets/Source_Code/Enemy.cs
+++ Assets/Source_Code/Enemy.cs
@@ -48,7 +48,7 @@ public class Enemy : Character
     protected override void OnCollisionStay(Collision collision)
     {
         base.OnCollisionStay(collision);
-        if (collision.contacts.Length > 4)
+        if (collision.contacts.Length > 4) //À revoir peut être éventuellement faire des classe pour chaque monstre et ajusté la valeur en conséquence du collider du monstre
             base.Jump();
     }
 
