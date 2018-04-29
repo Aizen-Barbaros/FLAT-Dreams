8e405bd8a79f7b408194f57e68b6c89ab5b5d763
Maxence Cote <maxencecote08@hotmail.com>
2018-04-29 13:08:32 -0400

    Adding animation on Agent

--- Assets/Source_Code/World.cs
+++ Assets/Source_Code/World.cs
@@ -208,7 +208,9 @@ public class World : MonoBehaviour
         // CHUNKS
         this.chunks = new Chunk[mapSize / chunkSize, mapSize / chunkSize];
 
+
         this.GenerateWorldValues(Random.Range(0, 10));
+
         this.GenerateTerrainValues(Random.Range(0, 3));
         this.GenerateSurfaceHeights();
         this.GenerateTerrain();
