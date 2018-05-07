da3b3ec9f17f0451b1ff00fa3d57328f7a0754f9
FelixTG <33939889+FelixTG@users.noreply.github.com>
2018-05-07 08:35:09 -0400

    ennemy sound

--- Assets/Source_Code/Enemy.cs
+++ Assets/Source_Code/Enemy.cs
@@ -66,7 +66,7 @@ public class Enemy : Character
         }
         if (!source.isPlaying && Time.time - 3 >= lastBaseSound)
         {
-            base.source.PlayOneShot(BaseSound, 0.13f);
+            base.source.PlayOneShot(BaseSound, 0.8f);
         }
 
         base.Move(target);
